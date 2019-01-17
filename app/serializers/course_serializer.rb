959941a9cdfac04b7c92a2449e712a7db4c85fa0
missnadia <miss.nadia.lee@gmail.com>
2019-01-17 13:34:42 -0500

    Update serializer associations

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,6 +1,7 @@
 class CourseSerializer < ActiveModel::Serializer
-  attributes :id, :name, :time_spent, :completed, :date, :student_id
-  has_many :students, through: :course_students
+  attributes :id, :name, :time_spent, :completed, :date, :student_id, :comment
+  has_many :course_students
+  has_many :students
   has_many :comments
   belongs_to :student
 end
--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,6 +1,6 @@
 class StudentSerializer < ActiveModel::Serializer
   attributes :id, username, :password, :email, :first_name, :last_name, :admin, :name
   has_many :course_students
-  has_many :courses through: course_students
-  has_many :comments, through: :courses
+  has_many :courses
+  has_many :comments
 end
